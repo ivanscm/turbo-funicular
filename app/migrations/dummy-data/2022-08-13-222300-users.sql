ee4777b4496da129420d61c0b753652889ce4f4c
IvanSCM <ivanscm@gmail.com>
2022-08-13 22:25:44 +0700

    #3 добавлены миграции тестовых данных БД

---
+++ app/migrations/dummy-data/2022-08-13-222300-users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, title) VALUES (1, 'Вася Пупкин');
+INSERT INTO users (id, title) VALUES (2, 'Петя Какашкин');
+INSERT INTO users (id, title) VALUES (3, 'Тазик Огуров');
+INSERT INTO users (id, title) VALUES (4, 'Газон Засеян');
