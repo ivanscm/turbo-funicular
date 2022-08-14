e2837d618c0160da2535c5eee735ba08244e69f9
IvanSCM <ivanscm@gmail.com>
2022-08-14 18:46:48 +0700

    #5 добавлен контроллер для вывода постов

---
+++ app/app/Presenters/PostsPresenter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use App\Models\PostsRepository;
+use Tracy\Debugger;
+
+
+final class PostsPresenter extends Nette\Application\UI\Presenter
+{
+    /** @var PostsRepository @inject */
+    public PostsRepository $postsRepository;
+
+    public function renderListAndLastThreeComments()
+    {
+        // LATERAL METHOD
+        Debugger::timer('postsByLateralMethod');
+        $this->template->postsByLateralMethod = $this->postsRepository->findPostsAndLastComments(3, PostsRepository::LATERAL_METHOD);
+        $this->template->postsByLateralMethodTime = Debugger::timer('postsByLateralMethod');
+
+        // JSON GROUP METHOD
+        Debugger::timer('postsByJsonGroupMethod');
+        $this->template->postsByJsonGroupMethod = $this->postsRepository->findPostsAndLastComments(3, PostsRepository::JSON_GROUP_METHOD);
+        $this->template->postsByJsonGroupMethodTime = Debugger::timer('postsByJsonGroupMethod');
+
+        // TWO QUERIES METHOD
+        Debugger::timer('postsByTwoQueriesMethod');
+        $this->template->postsByTwoQueriesMethod = $this->postsRepository->findPostsAndLastComments(3, PostsRepository::TWO_QUERIES_METHOD);
+        $this->template->postsByTwoQueriesMethodTime = Debugger::timer('postsByTwoQueriesMethod');
+
+    }
+}
